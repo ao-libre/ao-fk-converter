8678f5995641f67748c79894977619822acd1fb2
Lucas <lucas@Lucass-MacBook-Pro.local>
2018-09-11 13:53:09 +1200

    Added source code and binary

---
+++ GenerarFK/Metodos.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: XX1
+ * Fecha: 22/10/2016
+ * Hora: 01:20 a.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace GenerarFK
+{
+	public partial class Program
+	{
+		private static string fkFile = @"INIT/FK.ind";
+		private static tCabecera miCabecera;
+		private static int numeroMapas;
+		private static List<int> listaMapas;
+		
+		public static tCabecera GenerarCabezera(BinaryReader rFile)
+		{
+			tCabecera retval;
+			
+			retval.desc = rFile.ReadChars(255);
+			retval.crc = rFile.ReadInt32();
+			retval.magicWord = rFile.ReadInt32();
+			
+			return retval;
+		}
+		
+		public static bool ExistenFK()
+		{
+			return File.Exists(fkFile);
+		}
+		
+		public static bool EOF(StreamReader rFile, out string c)
+		{
+			return (c = rFile.ReadLine()) != null;
+		}
+		
+		public static void CrearFK()
+		{
+			StreamReader rFile;
+			BinaryWriter wFile;
+			string tmp;
+			short max = 400; 
+			
+			if(!File.Exists(@"INIT/FK.txt"))
+			{
+				Console.WriteLine("No existe el archivo FK.txt");
+				return;
+			}
+			
+			try
+			{
+				rFile  = new StreamReader(@"INIT/FK.txt");
+				wFile = new BinaryWriter(File.Create(fkFile));
+				
+				wFile.Write( new char[255]);
+				wFile.Write(99);
+				wFile.Write(33);
+				
+				wFile.Write(max);
+				
+				while(EOF(rFile,out tmp))
+				{
+					wFile.Write(Convert.ToByte(tmp));
+				}
+				rFile.Close();
+				wFile.Close();
+			}
+			catch(Exception)
+			{
+				Console.WriteLine("Error al crear el archivo FK.ind");
+			}
+		}
+		
+		public static void GenerarFK()
+		{
+			BinaryReader rFile;
+			StreamWriter wFile;
+			byte tmp;
+			
+			try
+			{
+				rFile = new BinaryReader(File.OpenRead(fkFile));
+				wFile = new StreamWriter(@"INIT/FK.txt");
+				miCabecera = GenerarCabezera(rFile);
+				numeroMapas = (int)rFile.ReadInt16();
+				
+				for(int x = 1; x <= numeroMapas; x++)
+				{
+					tmp = rFile.ReadByte();
+					wFile.WriteLine(tmp);
+				}
+				
+				rFile.Close();
+				wFile.Close();
+				Console.Beep();
+			}
+			catch(Exception)
+			{
+				Console.WriteLine("No se logro crear el archivo fk");
+			}
+		}
+	}
+}
---
+++ GenerarFK/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: XX1
+ * Fecha: 22/10/2016
+ * Hora: 01:09 a.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.IO;
+using AO;
+
+namespace GenerarFK
+{
+	public struct tCabecera
+	{
+		public char[] desc;
+		public int crc;
+		public int magicWord;
+	}
+	
+	public partial class Program
+	{
+		public static void Main(string[] args)
+		{
+			int opcion = -1;
+			if (!ExistenFK())
+			{
+				Console.WriteLine("No se logro encontrar el archivo FK.ind");
+				Console.ReadKey(true);
+				return;
+			}
+			
+			
+			do
+			{
+				try
+				{
+					Console.WriteLine();
+					Console.WriteLine();
+					
+					Console.WriteLine("Selecione una opcion:");
+					Console.WriteLine("1: Generar fk.txt");
+					Console.WriteLine("2: Generar fk.ind");
+					Console.WriteLine("3: Salir");
+					Console.Write(">> ");
+					opcion = Convert.ToInt32(Console.ReadLine());
+					
+					switch(opcion)
+					{
+						case 1:
+							GenerarFK();
+							break;
+							
+						case 2:
+							CrearFK();
+							break;
+							
+						case 3:
+							Console.WriteLine("Adios!");
+							break;
+							
+					}
+				}
+				catch(Exception)
+				{
+					Console.WriteLine("Opcion invalida.");
+				}
+				
+			}while(opcion != 3);
+			
+			Console.ReadKey(true);
+		}
+	}
+}
